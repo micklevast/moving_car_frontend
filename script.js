79dd60ab57926e2f6ddaf750c4ee4c6d81a532dc
vishal <visindra2000@gmail.com>
2022-02-05 20:37:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// Create new car_sound Element
+// var car_sound = document.createElement('car_sound');
+
+// // set the attributes for car_sound element 
+// car_sound.setAttribute('src', 'sound.mp3'); 
+// car_sound.loop=true;
+// car_sound.play();
+// Append car_sound element to HTML head
+// head.appendChild(car_sound); 
+
+
+
+// --------------------------------
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3'); 
+audio.loop=true;
+// audio.play();
+
+// ----------------------
+// don't no wht it is not working properly
